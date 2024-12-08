7a347c77e8e3aa607f7815c77385119fea5ebc5e
rehabmahmoud <rehabmahmoud521@gmail.com>
2024-12-08 23:10:06 +0200

    Structure modification

--- src/Library.java
+++ src/Library.java
@@ -2,9 +2,10 @@ package src;
 
 import java.util.ArrayList;
 import java.util.List;
-
+//Don't forget to have a creator class to initiate the Library
 public class Library {
      Librarian Clark;
+     //This could be modified to have a BookList Class so deletion and insertion will be through
      ArrayList<Book> Books;
      ArrayList<User> Clients;
      public List<Book> Search(String name)
@@ -26,18 +27,27 @@ public class Library {
         }
         return false;
     }
-     public boolean CreateBook(String summary,String title, String genre,String publisher,int ISBN, int pages)
+    public boolean AddBook(String summary,String title, String genre,String publisher,int ISBN, int pages)
+    {
+        Book newBook=CreateBook(summary, title, genre, publisher, ISBN, pages);
+        if(newBook != null) {
+            Books.add(newBook);
+            return true;
+        }
+        else return false;
+    }
+    //Should be modified to check that the current used is a librarian and not a normal user, Also which class should be responsible for creation
+     public Book CreateBook(String summary,String title, String genre,String publisher,int ISBN, int pages)
      {
          if(!SearchByIsbn(ISBN)) {
              BookDescription newBookDescription = new BookDescription(summary, title, genre, publisher, ISBN, pages);
              Book newBook = new Book();
              newBook.setDescription(newBookDescription);
-             Books.add(newBook);
-             return true;
+             return newBook;
          }
          else
          {
-             return false;
+            return null;
          }
      }
 }
