17002eb905e38d5ace4d3cfd44b1e6b8668bc826
Mohaned Atef <s-mohaned.hassan@zewailcity.edu.eg>
2024-12-24 16:05:44 +0200

    trying to test borrowedbooks in html

--- src/main/java/com/example/librarymanagementsystem/controller/BorrowedBookController.java
+++ src/main/java/com/example/librarymanagementsystem/controller/BorrowedBookController.java
@@ -1,8 +1,32 @@
 package com.example.librarymanagementsystem.controller;
 
+
+
+import com.example.librarymanagementsystem.model.dto.BorrowedBookDTO;
+import com.example.librarymanagementsystem.service.BorrowService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 /**
  * Controller class for managing the book list in the library.
  */
 
+@RestController
+@RequestMapping("/api/borrowed-books")
 public class BorrowedBookController {
+
+    @Autowired
+    private BorrowService borrowedBookService;
+
+    @GetMapping("/user")
+    public String getBorrowedBooksByUser(@RequestParam Integer userID, Model model) {
+        List<BorrowedBookDTO> borrowedBooks = borrowedBookService.getBorrowedBooksByUserID(userID);
+        model.addAttribute("userID", userID);
+        model.addAttribute("borrowedBooks", borrowedBooks);
+        return "borrowed_books";
+    }
 }
