17c8672fc97882942952a3a33c1e8b197448ce25
Mohaned Atef <s-mohaned.hassan@zewailcity.edu.eg>
2024-12-31 00:58:48 +0200

    added userID to session ID

--- src/main/java/com/example/librarymanagementsystem/controller/BorrowedBookController.java
+++ src/main/java/com/example/librarymanagementsystem/controller/BorrowedBookController.java
@@ -1,12 +1,8 @@
 package com.example.librarymanagementsystem.controller;
 
-
-
 import com.example.librarymanagementsystem.model.dto.BorrowedBookDTO;
 import com.example.librarymanagementsystem.model.dto.ReservedBookDTO;
-import com.example.librarymanagementsystem.model.entity.ReservedBook;
 import com.example.librarymanagementsystem.service.BorrowService;
-
 import com.example.librarymanagementsystem.service.FineManagement;
 import com.example.librarymanagementsystem.service.ReserveService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,37 +11,44 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
-
-/**
- * Controller class for managing the book list in the library.
- */
 
 @Controller
 @RequestMapping("/borrowed-books")
+@SessionAttributes("loggedInUserID")
 public class BorrowedBookController {
 
     @Autowired
     private BorrowService borrowedBookService;
+
     @Autowired
     private FineManagement fineManager;
+
     @Autowired
     private ReserveService reserveService;
 
-    @GetMapping("/user")
-    public String getBooksAndFinesByUser(@RequestParam("userID") Integer userID, Model model) {
+    @GetMapping
+    public String getBooksAndFinesByUser(@SessionAttribute("loggedInUserID") Integer userID, Model model) {
         if (userID == null) {
             model.addAttribute("errorMessage", "User ID is required.");
             return "error";
         }
+
         System.out.println("Fetching data for userID: " + userID);
+
+        // Fetch borrowed books
         List<BorrowedBookDTO> borrowedBooks = borrowedBookService.getBorrowedBooksByUserID(userID);
-        model.addAttribute("userID", userID);
         model.addAttribute("borrowedBooks", borrowedBooks);
+
+        // Fetch fine details
         List<BorrowedBookDTO> fineDetails = borrowedBookService.getBorrowedBooksByUserID(userID);
         model.addAttribute("fineDetails", fineDetails);
+
+        // Fetch reserved books
         List<ReservedBookDTO> reservedBooks = reserveService.getReservedBooksbyUserID(userID);
         model.addAttribute("reservedBooks", reservedBooks);
+
+        model.addAttribute("userID", userID);
+
         return "borrowed_books";
     }
 }
